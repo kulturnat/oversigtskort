1bfd7899d3c1c08c80290f6c129cdc44adb8a1ad
Henning Wangerin <github.com@henning.wangerin.dk>
2022-06-28 21:46:24 +0200

    Update afspærring.js

--- js/afspærring.js
+++ js/afspærring.js
@@ -20,13 +20,14 @@ line_to(        [54.911011, 9.7866]     );
 line_to(        [54.910485, 9.786689]   );
 line_to(        [54.91031, 9.78643]     );
 // Kirke bakken
-line_to(        [54.91024, 9.78650]     );
-line_to(        [54.909377, 9.786265]   );
+line_to(        [54.91024, 9.78650]     );      // Kirkebakken / Søndergade
+line_to(        [54.91018, 9.786563]    );      // Søndergade / Stengade
 line_to(        [54.909227, 9.786243]   );
 line_to(        [54.908732, 9.786233]   );
-line_to(        [54.908455, 9.78605]    );
-line_to(        [54.908179, 9.786893]   );
-line_to(        [54.907186, 9.786324]   );
+line_to(        [54.908455, 9.78605]    );      // Bagergade / Søndergade
+line_to(        [54.908179, 9.786893]   );      // Bagergade / Bjerggade
+line_to(        [54.907782, 9.787279]   );      // Brogade / Christian II's gade
+line_to(        [54.907186, 9.786324]   );      // Christian II's gade / Slotsgade / Slotsbakken
 line_to(        open1                   );
 L.polyline([    open2,
                 [54.90533, 9.785331]    ],{color:"red",         weight:5})
